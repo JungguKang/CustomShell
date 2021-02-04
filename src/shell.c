1c986760b8f922eeb4bc01839e21458ed7a99a90
junggu96 <justin403502@naver.com>
2021-02-04 23:08:05 +0900

    batchmode 구현완료
    1. batch mode  구현완료
    2. 함수 가독성 좋게

--- src/shell.c
+++ src/shell.c
@@ -4,8 +4,9 @@
 #include <sys/wait.h>
 
 
-void ExecBatchFile(char* argv[]);
-void ExecCmdLine();
+void BatchMode(char* argv[]);
+void InteractiveMode();
+void ExecSyscall(char * command);
 //char * ParseWithDelimeter(char * buf, char * del)[10];
 
 
@@ -13,19 +14,30 @@ int main(int argc, char* argv[])
 {
     if(argc > 1)
     {
-        ExecBatchFile(argv);
+        BatchMode(argv);
     }
     else
-        ExecCmdLine();
+        InteractiveMode();
 }
 
-void ExecBatchFile(char* argv[])
+void BatchMode(char* argv[])
 {
     FILE *fp = fopen(argv[1], "r");
+    if(fp == NULL)
+    {
+        printf("file can not be opened\n");
+        return;
+    }
 
+    while(!feof(fp))
+    {
+        char buffer[100];
+        fgets(buffer, sizeof(buffer), fp);
+        ExecSyscall(buffer);
+    }
 }
 
-void ExecCmdLine()
+void InteractiveMode()
 {
     char buffer[100];
     while(1)
@@ -38,47 +50,50 @@ void ExecCmdLine()
             return;
         }
 
-        //char * splitedCmd[10] = ParseWithDelimeter(buffer, ";");
-        int cmdPids[10];
-
-        char *splitedCmd[10];
-        splitedCmd[0] = strtok(buffer, ";\n");
-        char *temp;
-        int i;
-
-        for(i=1; (temp = strtok(NULL, ";\n")) != NULL ; i++)
-            splitedCmd[i] = temp;
-
-        splitedCmd[i] = 0;
-
-        int status;
-        int pid[10];
-
-
-       for(i=0; splitedCmd[i] != NULL ; i++)
-       {
-            char *splitedStr[10];
-            splitedStr[0] = strtok(splitedCmd[i], " ");
-            char *temp;
-            int j;
-
-            for(j=1; (temp = strtok(NULL, " ")) != NULL ; j++)
-                splitedStr[j] = temp;
-
-            splitedStr[j] = 0;
-
-            pid[i] = fork();
-
-            if(pid[i] == 0 )
-            {
-                if(execvp(splitedStr[0], splitedStr) == -1)
-                    printf("execvp not activated\n");                     
-            } 
-       }
-
-       
-        for(int j = 0; j <=i; j++)
-            waitpid(pid[j], &status, 0);
-       
+        ExecSyscall(buffer);   
     }
+}
+
+void ExecSyscall(char * command)
+{
+    int cmdPids[10];
+
+    char *splitedCmd[10];
+    splitedCmd[0] = strtok(command, ";\n");
+    char *temp;
+    int i;
+
+    for(i=1; (temp = strtok(NULL, ";\n")) != NULL ; i++)
+        splitedCmd[i] = temp;
+
+    splitedCmd[i] = 0;
+
+    int status;
+    int pid[10];
+
+
+    for(i=0; splitedCmd[i] != NULL ; i++)
+    {
+        char *splitedStr[10];
+        splitedStr[0] = strtok(splitedCmd[i], " ");
+        char *temp;
+        int j;
+
+        for(j=1; (temp = strtok(NULL, " ")) != NULL ; j++)
+            splitedStr[j] = temp;
+
+        splitedStr[j] = 0;
+
+        pid[i] = fork();
+
+        if(pid[i] == 0 )
+        {
+            if(execvp(splitedStr[0], splitedStr) == -1)
+                    printf("execvp not activated\n");                     
+        } 
+    }
+
+       
+    for(int j = 0; j <=i; j++)
+        waitpid(pid[j], &status, 0);
 }
